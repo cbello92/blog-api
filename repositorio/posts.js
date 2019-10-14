8a5e94aa8a891a908f8d4ceb774295caddc79e8f
camilo.bello92@outlook.es <camilo.bello92@outlook.es>
2019-10-14 00:30:03 -0300

    trabajando con express

---
+++ repositorio/posts.js
@@ -0,0 +1,19 @@
+const data = require('./resources/posts.json');
+
+const getAll = () => {
+    return data;
+}
+
+const getById = (id) => {
+    return data.find(x => x.id === id) || undefined;
+}
+
+const getByUserId = (userid) => {
+    return data.filter(x => x.userId === userid);
+}
+
+module.exports = {
+    getAll,
+    getById,
+    getByUserId
+}
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,27 @@
 const router = require('express').Router();
 const { getAll } = require('../repositorio/users');
+const { getByUserId } = require('../repositorio/posts');
 
 router.get('/', (req, res) => {
     try {
-        const pacientes = getAll();
-        res.send(pacientes);
+        let usuarios = getAll();
+        if (req.query && req.query.fields) {
+            let splitQuery = req.query.fields.split(',');
+            console.log(splitQuery.length);
+
+            if (splitQuery.length > 0) {
+                for (let i = 0; i < splitQuery.length; i++) {
+                    if (splitQuery[i] === 'posts') {
+                        for (let j = 0; j < usuarios.length; j++) {
+                            usuarios[j].posts= getByUserId(usuarios[j].id);
+                        }
+                    }
+                }
+            }
+
+            splitQuery = [];
+        }
+        res.status(200).send(usuarios);
     } catch (err) {
         res.status(500).send(err);
     }
